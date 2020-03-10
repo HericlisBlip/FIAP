72bad6e572a4d76ad9ecc0bc673db594b3f82584
Logon Aluno <logonaluno@local.com>
2020-03-10 10:40:59 -0300

    Adicionando tarefas de java no repositório remoto

--- java/Conta.java
+++ java/Conta.java
@@ -4,4 +4,19 @@ class Conta {
     double saldo ;
     double limite;
     //..
+
+	void sacar(double quantidade){
+		double novoSaldo =this.saldo - quantidade;
+		this.saldo = novoSaldo;
+		System.out.println("Saque realizado R$: " + quantidade);
+	}
+
+	void depositar(double quantidade){
+		this.saldo = this.saldo + quantidade;
+		System.out.println("Você depositou R$: " + quantidade);
+
+
+
+}
+
 }
