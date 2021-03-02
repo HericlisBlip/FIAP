cd718c6e0792521b7afdb5e152c97206a7efcdae
HericlisBlip <hericlisventura.o@gmail.com>
2021-03-02 09:57:55 -0300

    Removendo arquivos desnecessários.

--- java/Programa.java
+++
@@ -1,9 +0,0 @@
-class Programa{
-	public static void main(String[]args){
-		Conta minhaConta; // variavel
-		minhaConta = new Conta();
-		minhaConta.dono = "Hericlis";
-		minhaConta.saldo = 10000.0;
-		System.out.println("Saldo atual: "+ minhaConta.saldo);
-	}
-}
